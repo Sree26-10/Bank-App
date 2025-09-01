2bd7bce1d276e7f08734dc9997c766cfbc08f888
Sree <sreehari.kammari@gmail.com>
2025-09-01 20:19:30 +0530

    #Added Dto classes to all the entities #Apis needs to be added

---
+++ accounts/src/main/java/com/upskill/accounts/constants/AccountsConstants.java
@@ -0,0 +1,4 @@
+package com.upskill.accounts.constants;
+
+public class AccountsConstants {
+}
--- accounts/src/main/java/com/upskill/accounts/repository/CustomerRepository.java
+++ accounts/src/main/java/com/upskill/accounts/repository/CustomerRepository.java
@@ -1,6 +1,9 @@
 package com.upskill.accounts.repository;
 
-
 import com.upskill.accounts.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
