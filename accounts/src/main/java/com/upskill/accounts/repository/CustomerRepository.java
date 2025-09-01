913e9bc7153cdef3895b8cc6de6e12a460dce52f
Sree26-10 <sreehari.kammari@gmail.com>
2025-09-01 11:27:30 +0530

    Initial commit: Bank application setup

---
+++ accounts/src/main/java/com/upskill/accounts/repository/CustomerRepository.java
@@ -0,0 +1,6 @@
+package com.upskill.accounts.repository;
+
+
+import com.upskill.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
